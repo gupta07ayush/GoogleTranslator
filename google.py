2d336a4c19e1a8b3535674c9a3b9e435c2d3a021
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-19 23:03:16 +0530

    added destination label and text area

--- google.py
+++ google.py
@@ -7,7 +7,7 @@ root.geometry('500x700')  # set the default opening window size
 root.config(bg='red') #set background color red
 
 lab_txt = Label(root, text = 'Translator', font=("Times New Roman", 40, 'bold')) # creating a header label
-lab_txt.place(x=100,y=40,height=50,width=300) # placing this lable on the top
+lab_txt.place(x=100,y=30,height=50,width=300) # placing this lable on the top
 
 frame = Frame(root,).pack(side=BOTTOM) #creating frame, then put out data inside frame, we place frame using pack
 
@@ -31,6 +31,13 @@ comb_dest = ttk.Combobox(frame, value=list_text)  # destination language combo b
 comb_dest.place(x=330,y=300,height=40,width=150)
 comb_dest.set("Hindi8")  # set default lang.
 
+
+# Label and text area for destination
+lab_txt = Label(root, text = 'Destination Text', font=("Times New Roman", 17, 'bold'), fg='black', bg = 'red') # label for source text
+lab_txt.place(x=100,y=375,height=20,width=300) # placing this lable on the text area
+
+dest_txt = Text(frame, font=("vardana", 20, 'bold'), wrap=WORD )  # making a text field where user will put his text which he wants to translate
+dest_txt.place(x=10,y=400,height=150,width=480)
 
 
 
